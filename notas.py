cd37d38a6cb885da16191a67fbc39590c20001bb
Gustavo <inaciogustavo972@gmail.com>
2026-04-01 11:02:40 -0300

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,7 @@
+primeira_nota = float(input("Digite a primeira nota: "))
+segunda_nota = float(input("Digite a segunda nota: "))
+terceira_nota = float(input("Digite a terceira nota: "))
+resultado = (primeira_nota + segunda_nota + terceira_nota)/3
+print(f"o resultado e {resultado}")
+
+#--Gustavo inacio de Oliveira--
