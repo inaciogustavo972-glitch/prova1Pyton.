ef5ab17ef8e6fe8688a131831cc508c20a0973be
Gustavo <inaciogustavo972@gmail.com>
2026-04-01 10:05:46 -0300

    Add files via upload

---
+++ verificador.py
@@ -0,0 +1,7 @@
+usuario = input("Qual o nome do usuario? ")
+if usuario == "admin":
+    print("Acesso total liberado!")
+else:
+    print("Acesso limitado concedido!")
+
+#--Gustavo inácio de Oliveira--
