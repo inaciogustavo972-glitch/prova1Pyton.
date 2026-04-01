9ddc5b091d0d07e9d56eba8fbf1c5e45cfcdadcd
Gustavo <inaciogustavo972@gmail.com>
2026-04-01 10:51:35 -0300

    Add files via upload

---
+++ lanches.py
@@ -0,0 +1,9 @@
+comida = float(input("Coloque o preço: "))
+if comida <= 10.00:
+    print("Preço justo")
+elif comida <= 20.00:
+    print("Esta ficando muito caro!")
+else:
+    print("Muito caro! melhor levar o de casa")
+
+#--Gustavo inacio de Oliveira--
